f6fe7e13ba6d1f2bbc3d29caea461268dfc18966
baiziyu-fe <baiziyu-fe@outlook.com>
2022-06-25 22:16:50 +0800

    feat(types): add Protagonist interface && base demo info

---
+++ src/info.d.ts
@@ -0,0 +1,66 @@
+/**补充描述 */
+interface ISupplementaryDescription {
+  title: string;
+  desc: string;
+}
+
+interface IBaseInfo {
+  start: string;
+  end?: string;
+}
+
+interface IBaseExperience extends IBaseInfo {
+  name: string;
+  nameEN?: string;
+  supplementaryDescription?: ISupplementaryDescription[]
+}
+
+interface ICompanyExperience extends IBaseExperience {
+  position: string;
+  mainWork: string[];
+  mainResult: string[];
+}
+
+interface IAward {
+  time: string;
+  title: string;
+  desc?: string;
+}
+
+interface IEducationalExperience extends IBaseExperience {
+  award?: IAward[];
+  course?: string[];
+  schoolType?: string[];
+}
+
+interface ISkill {
+  title: string;
+  mastery: string;
+}
+
+/**作品 */
+interface IWork extends IBaseInfo {
+  title: string;
+  description: string;
+  technology?: string;
+  previewAddress?: string;
+  technicalHighlights?: string[];
+  result?: string[];
+}
+
+/**主角信息 */
+interface IProtagonistInfo {
+  name: string;
+  birthYear: number;
+  position?: string;
+  currentCompany?: string;
+  currentCompanyEN?: string;
+  /**工作经历 */
+  companyExperience?: ICompanyExperience[];
+  /**教育经历 */
+  educationalExperience?: IEducationalExperience[];
+  skill?: ISkill[];
+  hobby?: string[];
+  /**作品 */
+  work?: IWork[];
+}
---
+++ src/protagonist.ts
@@ -0,0 +1,13 @@
+const cardInfo: IProtagonistInfo  = {
+  name: '白子煜',
+  birthYear: 2000,
+  position: '前端开发工程师',
+  currentCompany: '字节跳动',
+  currentCompanyEN: 'ByteDance',
+  educationalExperience: [],
+  skill: [],
+  hobby: [],
+  work: [],
+};
+
+export default cardInfo;

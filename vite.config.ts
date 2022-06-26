981d1de7ad9418942a1939ea19b2f0fd91a0cd31
baiziyu-fe <baiziyu-fe@outlook.com>
2022-06-26 14:16:02 +0800

    feat(resolve-path): add resolve path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: "/character-card/",
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.js', '.json', '.ts', '.vue'],
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+      { find: "components", replacement: path.resolve(__dirname, './src/components') },
+      { find: "hooks", replacement: path.resolve(__dirname, './src/hooks') },
+      { find: "types", replacement: path.resolve(__dirname, './src/types') },
+      { find: "utils", replacement: path.resolve(__dirname, './src/utils') },
+    ]
+  },
 })
